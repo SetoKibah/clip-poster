d5b655359cc1a731295a4987018889bff30ee49e
SetoKibah <setokibah@gmail.com>
2023-09-04 14:43:05 -0600

    Forgot to save two files, minor change.

--- src/tests/test_clip_generation.py
+++ src/tests/test_clip_generation.py
@@ -1,6 +1,14 @@
 # tests/test_clip_generation.py
 
 import unittest
+import logging
+
+def setup_logging():
+    logging.basicConfig(filename="app.log",
+                        level=logging.INFO,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+ 
 # Import necessary functions and classes when ready
 
 class TestClipGenerator(unittest.TestCase):
