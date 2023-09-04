866b28ca87e28e65a42dd0810a8b49b7977d13e3
SetoKibah <setokibah@gmail.com>
2023-09-04 14:21:04 -0600

    Implemented current work flow, still requires API integration and upload pathing. May remove Clip generation in favor of speed.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
-# imports
-from utils import CustomError, some_helper_function, VideoProcessor
+# main.py
 
+# imports
+from clip_generation import ClipGenerator
+from clip_processing import ClipProcessor
+from metadata_collection import MetadataCollector
 
 # Run program
 def main():
-    
-    # Using utils example
-    processor = VideoProcessor("path_to_video")
-    processor.resize("1080p")
+    # Generate a clip
+    generator = ClipGenerator("path_to_video.mp4")
+    clip_path = generator.extract_clip(10,20) # Extract clip from 10s to 20s.
+    generator.close()
+
+    # Process the clip
+    processor = ClipProcessor(clip_path)
+    resized_clip_path =processor.resize((480, 320))
+    converted_clip_path = processor.convert_format("avi")
+    # Mock upload to demonstrate flow
+    upload_response = processor.mock_upload("Twitter")
+    processor.close()
+
+    # Collect metadata
+    collector = MetadataCollector(converted_clip_path)
+    metadata = collector.collect_all_metadata()
+    collector.close()
 
 if __name__ == "__main__":
     main()
